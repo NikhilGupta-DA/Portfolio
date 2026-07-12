5aa5aa4ed011a3f4d671b61878dec4f924544f8a
Nikhil Gupta <nikhil.gupta.21221@gmail.com>
2026-07-12 16:58:12 +0530

    Migrate from deprecated gsap-trial to free standard gsap package

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,6 +1,6 @@
 /// <reference types="vite/client" />
 
-declare module "gsap-trial/SplitText" {
+declare module "gsap/SplitText" {
   export class SplitText {
     constructor(target: string | HTMLElement | Element | (string | HTMLElement | Element)[], vars?: unknown);
     chars: HTMLElement[];
@@ -11,7 +11,7 @@ declare module "gsap-trial/SplitText" {
   }
 }
 
-declare module "gsap-trial/ScrollSmoother" {
+declare module "gsap/ScrollSmoother" {
   export class ScrollSmoother {
     static create(vars: unknown): ScrollSmoother;
     static get(): ScrollSmoother;
