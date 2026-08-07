d226308fbfdfebab177d45e45d19d81126cddeb2
Nikhil Gupta <nikhil.gupta.21221@gmail.com>
2026-08-08 01:18:27 +0530

    Fix borders cutting off in What I Do component and remove the fade gradient overlay from landing-info-h2

--- src/components/WhatIDo.tsx
+++ src/components/WhatIDo.tsx
@@ -51,18 +51,18 @@ const WhatIDo = () => {
           <div className="what-border2">
             <svg width="100%">
               <line
-                x1="0"
+                x1="1"
                 y1="0"
-                x2="0"
+                x2="1"
                 y2="100%"
                 stroke="white"
                 strokeWidth="2"
                 strokeDasharray="7,7"
               />
               <line
-                x1="100%"
+                x1="calc(100% - 1px)"
                 y1="0"
-                x2="100%"
+                x2="calc(100% - 1px)"
                 y2="100%"
                 stroke="white"
                 strokeWidth="2"
@@ -78,18 +78,18 @@ const WhatIDo = () => {
               <svg height="100%">
                 <line
                   x1="0"
-                  y1="0"
+                  y1="1"
                   x2="100%"
-                  y2="0"
+                  y2="1"
                   stroke="white"
                   strokeWidth="2"
                   strokeDasharray="6,6"
                 />
                 <line
                   x1="0"
-                  y1="100%"
+                  y1="calc(100% - 1px)"
                   x2="100%"
-                  y2="100%"
+                  y2="calc(100% - 1px)"
                   stroke="white"
                   strokeWidth="2"
                   strokeDasharray="6,6"
@@ -127,9 +127,9 @@ const WhatIDo = () => {
               <svg height="100%">
                 <line
                   x1="0"
-                  y1="100%"
+                  y1="calc(100% - 1px)"
                   x2="100%"
-                  y2="100%"
+                  y2="calc(100% - 1px)"
                   stroke="white"
                   strokeWidth="2"
                   strokeDasharray="6,6"
